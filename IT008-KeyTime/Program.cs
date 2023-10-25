fbdd70495dbfe6f90c6d2e4c34f7988af88e5869
Hoàng Hồ Quốc Bảo <22520102@gm.uit.edu.vn>
2023-10-25 20:55:23 +0700

    form login and sign up

--- IT008-KeyTime/Program.cs
+++ IT008-KeyTime/Program.cs
@@ -17,6 +17,8 @@ namespace IT008_KeyTime
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new Loginform());
+            Application.Run(new Registrationform());
         }
     }
 }
