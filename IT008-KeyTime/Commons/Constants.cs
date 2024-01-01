b8a2f6cdaaab0df08bd5ac319dbb1fce009b69f3
Duy Nguyen Hoang <hoangduy06104@gmail.com>
2024-01-01 18:56:18 +0900

    installer and default db link

--- IT008-KeyTime/Commons/Constants.cs
+++ IT008-KeyTime/Commons/Constants.cs
@@ -15,7 +15,7 @@ namespace IT008_KeyTime.Commons
         private const string password = "postgres";
         private const string dbName = "keytime";
 
-        //public const string PostgresConnection = "Host=ep-shrill-thunder-650028.us-east-2.aws.neon.tech;Username=hoangduy06104;Password=u2PnqmQ1WSXt;Database=keytime";
-        public const string PostgresConnection = "Host=" + host + ";Port=" + port +";Username=" + username + ";Password=" + password + ";Database=" + dbName;
+        public const string PostgresConnection = "Host=ep-shrill-thunder-650028.us-east-2.aws.neon.tech;Username=hoangduy06104;Password=u2PnqmQ1WSXt;Database=keytime";
+        //public const string PostgresConnection = "Host=" + host + ";Port=" + port +";Username=" + username + ";Password=" + password + ";Database=" + dbName;
     }
 }
