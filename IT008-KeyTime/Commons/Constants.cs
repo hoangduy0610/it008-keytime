1d907b1bec7410d195e1e11185813132d9baf8b0
Duy Nguyen Hoang <hoangduy06104@gmail.com>
2024-01-01 18:30:10 +0900

    update constants and readme

--- IT008-KeyTime/Commons/Constants.cs
+++ IT008-KeyTime/Commons/Constants.cs
@@ -8,6 +8,14 @@ namespace IT008_KeyTime.Commons
 {
     internal class Constants
     {
-        public const string PostgresConnection = "Host=ep-shrill-thunder-650028.us-east-2.aws.neon.tech;Username=hoangduy06104;Password=u2PnqmQ1WSXt;Database=keytime";
+        // Edit the Connection String here
+        private const string host = "localhost";
+        private const string port = "5432";
+        private const string username = "postgres";
+        private const string password = "postgres";
+        private const string dbName = "keytime";
+
+        //public const string PostgresConnection = "Host=ep-shrill-thunder-650028.us-east-2.aws.neon.tech;Username=hoangduy06104;Password=u2PnqmQ1WSXt;Database=keytime";
+        public const string PostgresConnection = "Host=" + host + ";Port=" + port +";Username=" + username + ";Password=" + password + ";Database=" + dbName;
     }
 }
